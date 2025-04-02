75df9e59fa9a0cd5c0aea9c629871e55abb90be3
Logan Brown <logan.brown@tylertech.com>
2025-04-02 13:13:12 -0400

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Il2CppDumper_GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dump = new Button();
+            SelectGameFolder = new Button();
+            SelectGameLabel = new Label();
+            GameDirectoryTextBox = new TextBox();
+            GameDirectoryBrowser = new FolderBrowserDialog();
+            OpenDllBrowser = new OpenFileDialog();
+            OpenMetaDataBrowser = new OpenFileDialog();
+            LogOutputTextBox = new RichTextBox();
+            OpenLogsFolder = new Button();
+            AutoOpenOutput = new CheckBox();
+            OpenOutput = new Button();
+            SuspendLayout();
+            // 
+            // dump
+            // 
+            dump.Location = new Point(364, 389);
+            dump.Name = "dump";
+            dump.Size = new Size(75, 23);
+            dump.TabIndex = 7;
+            dump.Text = "Dump";
+            dump.UseVisualStyleBackColor = true;
+            dump.Click += dump_Click;
+            // 
+            // SelectGameFolder
+            // 
+            SelectGameFolder.Location = new Point(12, 39);
+            SelectGameFolder.Name = "SelectGameFolder";
+            SelectGameFolder.Size = new Size(75, 23);
+            SelectGameFolder.TabIndex = 8;
+            SelectGameFolder.TabStop = false;
+            SelectGameFolder.Text = "Select";
+            SelectGameFolder.UseVisualStyleBackColor = true;
+            SelectGameFolder.Click += SelectGameFolder_Click;
+            // 
+            // SelectGameLabel
+            // 
+            SelectGameLabel.AutoSize = true;
+            SelectGameLabel.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            SelectGameLabel.Location = new Point(10, 9);
+            SelectGameLabel.Name = "SelectGameLabel";
+            SelectGameLabel.Size = new Size(127, 20);
+            SelectGameLabel.TabIndex = 9;
+            SelectGameLabel.Text = "Game Directory";
+            // 
+            // GameDirectoryTextBox
+            // 
+            GameDirectoryTextBox.BorderStyle = BorderStyle.FixedSingle;
+            GameDirectoryTextBox.Enabled = false;
+            GameDirectoryTextBox.Location = new Point(93, 39);
+            GameDirectoryTextBox.Name = "GameDirectoryTextBox";
+            GameDirectoryTextBox.ReadOnly = true;
+            GameDirectoryTextBox.Size = new Size(346, 23);
+            GameDirectoryTextBox.TabIndex = 10;
+            // 
+            // GameDirectoryBrowser
+            // 
+            GameDirectoryBrowser.RootFolder = Environment.SpecialFolder.Recent;
+            GameDirectoryBrowser.ShowNewFolderButton = false;
+            GameDirectoryBrowser.ShowPinnedPlaces = false;
+            GameDirectoryBrowser.HelpRequest += GameDirectoryBrowser_HelpRequest;
+            // 
+            // OpenDllBrowser
+            // 
+            OpenDllBrowser.FileName = "OpenDllBrowser";
+            OpenDllBrowser.FileOk += OpenDllBrowser_FileOk;
+            // 
+            // OpenMetaDataBrowser
+            // 
+            OpenMetaDataBrowser.FileName = "OpenMetaDataBrowser";
+            OpenMetaDataBrowser.FileOk += OpenMetaDataBrowser_FileOk;
+            // 
+            // LogOutputTextBox
+            // 
+            LogOutputTextBox.BackColor = SystemColors.Control;
+            LogOutputTextBox.BorderStyle = BorderStyle.FixedSingle;
+            LogOutputTextBox.Location = new Point(12, 87);
+            LogOutputTextBox.Name = "LogOutputTextBox";
+            LogOutputTextBox.ReadOnly = true;
+            LogOutputTextBox.ScrollBars = RichTextBoxScrollBars.Vertical;
+            LogOutputTextBox.Size = new Size(427, 296);
+            LogOutputTextBox.TabIndex = 11;
+            LogOutputTextBox.Text = "";
+            // 
+            // OpenLogsFolder
+            // 
+            OpenLogsFolder.Enabled = false;
+            OpenLogsFolder.Location = new Point(12, 389);
+            OpenLogsFolder.Name = "OpenLogsFolder";
+            OpenLogsFolder.Size = new Size(75, 23);
+            OpenLogsFolder.TabIndex = 13;
+            OpenLogsFolder.Text = "View Logs";
+            OpenLogsFolder.UseVisualStyleBackColor = true;
+            OpenLogsFolder.Click += OpenLogsFolder_Click;
+            // 
+            // AutoOpenOutput
+            // 
+            AutoOpenOutput.AutoSize = true;
+            AutoOpenOutput.Checked = true;
+            AutoOpenOutput.CheckState = CheckState.Checked;
+            AutoOpenOutput.FlatStyle = FlatStyle.Flat;
+            AutoOpenOutput.Location = new Point(142, 392);
+            AutoOpenOutput.Name = "AutoOpenOutput";
+            AutoOpenOutput.Size = new Size(135, 19);
+            AutoOpenOutput.TabIndex = 14;
+            AutoOpenOutput.Text = "Open when finished";
+            AutoOpenOutput.UseVisualStyleBackColor = true;
+            // 
+            // OpenOutput
+            // 
+            OpenOutput.Enabled = false;
+            OpenOutput.Location = new Point(283, 389);
+            OpenOutput.Name = "OpenOutput";
+            OpenOutput.Size = new Size(75, 23);
+            OpenOutput.TabIndex = 15;
+            OpenOutput.Text = "Open";
+            OpenOutput.UseVisualStyleBackColor = true;
+            OpenOutput.Click += OpenOutput_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(451, 419);
+            Controls.Add(OpenOutput);
+            Controls.Add(AutoOpenOutput);
+            Controls.Add(OpenLogsFolder);
+            Controls.Add(LogOutputTextBox);
+            Controls.Add(GameDirectoryTextBox);
+            Controls.Add(SelectGameLabel);
+            Controls.Add(SelectGameFolder);
+            Controls.Add(dump);
+            Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Main";
+            Text = "Il2CppDumper";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button dump;
+        private Button SelectGameFolder;
+        private Label SelectGameLabel;
+        private TextBox GameDirectoryTextBox;
+        private FolderBrowserDialog GameDirectoryBrowser;
+        private OpenFileDialog OpenDllBrowser;
+        private OpenFileDialog OpenMetaDataBrowser;
+        private RichTextBox LogOutputTextBox;
+        private Button OpenLogsFolder;
+        private CheckBox AutoOpenOutput;
+        private Button OpenOutput;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,343 @@
+using System.Diagnostics;
+
+namespace Il2CppDumper_GUI
+{
+    public partial class Main : Form
+    {
+        private string AssemblyPath { get; set; } = "";
+        private string MetadataPath { get; set; } = "";
+        private readonly string outputDir = Path.Combine(Directory.GetCurrentDirectory(), "output");
+        private readonly string dumpedDir = Path.Combine(Directory.GetCurrentDirectory(), "dumped");
+        private readonly string logsDir = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+        private string DataDir { get; set; } = "";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Check if output folder exists
+            if (Directory.Exists(outputDir))
+            {
+                OpenOutput.Enabled = true;
+            }
+
+            // Check if the logs folder exists
+            if (Directory.Exists(logsDir))
+            {
+                OpenLogsFolder.Enabled = true;
+            }
+        }
+
+        private void SelectGameFolder_Click(object sender, EventArgs e)
+        {
+            // Open the folder browser dialog
+            if (GameDirectoryBrowser.ShowDialog() == DialogResult.OK)
+            {
+
+                // Find the first folder that ends with "_Data" in the selected directory
+                // This is the data directory
+                string[] dirs = Directory.GetDirectories(GameDirectoryBrowser.SelectedPath);
+                foreach (string dir in dirs)
+                {
+                    if (dir.EndsWith("_Data"))
+                    {
+                        DataDir = dir;
+                        break;
+                    }
+                }
+
+                if (String.IsNullOrEmpty(DataDir))
+                {
+                    string message = "Invalid or unsupported game directory. Please select a valid game directory.";
+                    MessageBox.Show(message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                AssemblyPath = Path.Combine(GameDirectoryBrowser.SelectedPath, "GameAssembly.dll");
+                if (!File.Exists(AssemblyPath))
+                {
+                    string message = $"GameAssembly.dll could not be automatically detected in {GameDirectoryBrowser.SelectedPath}. Please select the DLL file manually.";
+                    MessageBox.Show(message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    OpenDllBrowser.InitialDirectory = GameDirectoryBrowser.SelectedPath;
+                    OpenDllBrowser.Filter = "DLL files (*.dll)|*.dll";
+                    OpenDllBrowser.ShowDialog();
+                }
+
+                MetadataPath = Path.Combine(DataDir, "il2cpp_data", "Metadata", "global-metadata.dat");
+                if (!File.Exists(MetadataPath))
+                {
+                    string message = $"global-metadata.dat could not be automatically detected in {GameDirectoryBrowser.SelectedPath}. Please select the metadata file manually.";
+                    MessageBox.Show(message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    OpenMetaDataBrowser.InitialDirectory = GameDirectoryBrowser.SelectedPath;
+                    OpenMetaDataBrowser.Filter = "Metadata files (*.dat)|*.dat";
+                    OpenMetaDataBrowser.ShowDialog();
+                }
+
+                GameDirectoryTextBox.Text = GameDirectoryBrowser.SelectedPath;
+            }
+        }
+
+        // Manually specify the path to the DLL
+        private void OpenDllBrowser_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (OpenDllBrowser.ShowDialog() == DialogResult.OK)
+            {
+                AssemblyPath = OpenDllBrowser.FileName;
+            }
+        }
+
+        // Manually specify the path to the metadata file
+        private void OpenMetaDataBrowser_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (OpenMetaDataBrowser.ShowDialog() == DialogResult.OK)
+            {
+                MetadataPath = OpenMetaDataBrowser.FileName;
+            }
+        }
+
+        private void GameDirectoryBrowser_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void dump_Click(object sender, EventArgs e)
+        {
+            dump.Enabled = false;
+            LogOutputTextBox.Text = "";
+
+            // Clear dumped directory
+            if (Directory.Exists(dumpedDir))
+            {
+                Directory.Delete(dumpedDir, true);
+            }
+
+            // Check for required paths
+            if (string.IsNullOrEmpty(MetadataPath) || string.IsNullOrEmpty(AssemblyPath))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("Please select the game directory, GameAssembly.dll, and global-metadata.dat file.",
+                                "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string dumperPath = Path.Combine(Directory.GetCurrentDirectory(), "binaries",
+                                Environment.Is64BitOperatingSystem ? "Il2CppDumper.exe" : "Il2CppDumper-x86.exe");
+
+            if (!File.Exists(dumperPath))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("Il2CppDumper not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check for config.json
+            string configPath = Path.Combine(Directory.GetCurrentDirectory(), "binaries", "config.json");
+            if (!File.Exists(configPath))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("config.json not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create dump output directory if it doesn't exist
+            Directory.CreateDirectory(dumpedDir);
+
+            string dumpArguments = $"\"{AssemblyPath}\" \"{MetadataPath}\" \"{dumpedDir}\"";
+
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = dumperPath,
+                Arguments = dumpArguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using Process process = new() { StartInfo = startInfo };
+            process.OutputDataReceived += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    LogOutputTextBox.Invoke((MethodInvoker)(() =>
+                    {
+                        LogOutputTextBox.AppendText(e.Data + Environment.NewLine);
+                        LogOutputTextBox.SelectionStart = LogOutputTextBox.Text.Length;
+                        LogOutputTextBox.ScrollToCaret(); // Ensure scrolling happens
+                    }));
+                }
+            };
+
+            process.ErrorDataReceived += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    LogOutputTextBox.Invoke((MethodInvoker)(() =>
+                    {
+                        LogOutputTextBox.AppendText("[ERROR] " + e.Data + Environment.NewLine);
+                        LogOutputTextBox.SelectionStart = LogOutputTextBox.Text.Length;
+                        LogOutputTextBox.ScrollToCaret(); // Ensure scrolling happens
+                    }));
+                }
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            await process.WaitForExitAsync(); // Asynchronous waiting to prevent UI freezing
+
+            // Create logs folder if it doesn't exist
+            Directory.CreateDirectory(logsDir);
+
+            // Write output to a log file
+            string logPath = Path.Combine(logsDir, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.log");
+
+            await File.WriteAllTextAsync(logPath, LogOutputTextBox.Text);
+
+            // Unhollow the dumped DLLs
+
+            // Create output directory if it doesn't exist
+            Directory.CreateDirectory(outputDir);
+            string dummyDllDir = Path.Combine(dumpedDir, "DummyDll");
+            string msCoreLibPath = Path.Combine(dummyDllDir, "mscorlib.dll");
+
+            // Check if dummy DLL directory exists
+            if (!Directory.Exists(dummyDllDir))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("DummyDll directory not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if mscorlib.dll exists
+            if (!File.Exists(msCoreLibPath))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("mscorlib.dll not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get the last directory of the DataDir
+            string gameDir = $"{DataDir.Split(Path.DirectorySeparatorChar).Last().Replace("_Data", "")}_Unhollowed";
+            string UnhollowerOutputDir = Path.Combine(outputDir, gameDir);
+            // Delete the output directory if it exists
+            if (Directory.Exists(UnhollowerOutputDir))
+            {
+                Directory.Delete(UnhollowerOutputDir, true);
+            }
+
+            // Create the output directory
+            Directory.CreateDirectory(UnhollowerOutputDir);
+
+            string UnhollowerArguments = $"--input=\"{dummyDllDir}\" --output=\"{UnhollowerOutputDir}\" --mscorlib=\"{msCoreLibPath}\"";
+            // Check if Unhollower exists
+            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "binaries", "AssemblyUnhollower.exe")))
+            {
+                dump.Enabled = true;
+                MessageBox.Show("AssemblyUnhollower not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ProcessStartInfo UnhollowerStartInfo = new()
+            {
+                FileName = Path.Combine(Directory.GetCurrentDirectory(), "binaries", "AssemblyUnhollower.exe"),
+                Arguments = UnhollowerArguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using Process UnhollowerProcess = new() { StartInfo = UnhollowerStartInfo };
+            UnhollowerProcess.OutputDataReceived += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    LogOutputTextBox.Invoke((MethodInvoker)(() =>
+                    {
+                        LogOutputTextBox.AppendText(e.Data + Environment.NewLine);
+                        LogOutputTextBox.SelectionStart = LogOutputTextBox.Text.Length;
+                        LogOutputTextBox.ScrollToCaret(); // Ensure scrolling happens
+                    }));
+                }
+            };
+
+            UnhollowerProcess.ErrorDataReceived += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    LogOutputTextBox.Invoke((MethodInvoker)(() =>
+                    {
+                        LogOutputTextBox.AppendText("[ERROR] " + e.Data + Environment.NewLine);
+                        LogOutputTextBox.SelectionStart = LogOutputTextBox.Text.Length;
+                        LogOutputTextBox.ScrollToCaret(); // Ensure scrolling happens
+                    }));
+                }
+            };
+
+            UnhollowerProcess.Start();
+            UnhollowerProcess.BeginOutputReadLine();
+            UnhollowerProcess.BeginErrorReadLine();
+
+            await UnhollowerProcess.WaitForExitAsync(); // Asynchronous waiting to prevent UI freezing
+
+            await File.WriteAllTextAsync(logPath, LogOutputTextBox.Text);
+
+            OpenOutput.Enabled = true;
+            dump.Enabled = true;
+            OpenLogsFolder.Enabled = true;
+
+            if (AutoOpenOutput.Checked)
+            {
+                if (Directory.Exists(outputDir))
+                {
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = "explorer.exe",
+                        Arguments = $"\"{UnhollowerOutputDir}\"",
+                        UseShellExecute = true
+                    });
+                }
+            }
+        }
+        private void OpenLogsFolder_Click(object sender, EventArgs e)
+        {
+            // Open the logs folder if it exists
+            if (Directory.Exists(logsDir))
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "explorer.exe",
+                    Arguments = $"\"{logsDir}\"",
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                MessageBox.Show("No logs found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void OpenOutput_Click(object sender, EventArgs e)
+        {
+            //  Open the output folder if it exists
+            if (Directory.Exists(outputDir))
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "explorer.exe",
+                    Arguments = $"\"{outputDir}\"",
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                MessageBox.Show("No output found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
